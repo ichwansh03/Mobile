a638e52777449727b787e40bee29ef703289bc94
ichwansh03 <ichwansholihin03@gmail.com>
2023-01-13 20:43:09 +0700

    Initial commit

---
+++ app/src/main/java/com/jrektor/mobile/admin/AddLaporanActivity.kt
@@ -0,0 +1,11 @@
+package com.jrektor.mobile.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddLaporanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_laporan)
+    }
+}
---
+++ app/src/main/java/com/jrektor/mobile/user/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.jrektor.mobile.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jrektor.mobile.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
